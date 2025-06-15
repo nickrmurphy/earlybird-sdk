655e6fbbe08b047ee9d8b4ad02ef02ae2f3410f6
nickrmurphy <hi@nickmurphy.io>
2025-06-15 08:56:49 -0500

    Remove v1 store benchmarks and implementation

--- packages/store/store/benchmarks/store-memory.bench.ts
+++
@@ -1,4 +0,0 @@
-import { createMemoryAdapter } from '../../storage/memory-adapter';
-import { createStoreBenchmarks } from './store.bench-utils';
-
-createStoreBenchmarks('Memory', createMemoryAdapter);
--- packages/store/store/utils/path.ts
+++
@@ -1,4 +0,0 @@
-export const getFilePath = (collection: string, id: string) =>
-	`${collection}/${id}.json`;
-
-export const getCollectionPath = (collection: string) => `${collection}/`;
