57f02b5d40732fecf477fa20d4d65bafe6b285df
nickrmurphy <hi@nickmurphy.io>
2025-06-15 08:59:12 -0500

    remove v1 storage

--- packages/store/storage/node-adapter.test.ts
+++
@@ -1,7 +0,0 @@
-// @vitest-environment node
-//
-import { createStorageAdapterTests } from './adapters.test-utils';
-import { createNodeFsAdapter } from './node-adapter';
-
-const nodeFsAdapter = createNodeFsAdapter();
-createStorageAdapterTests('NodeFS', nodeFsAdapter);
