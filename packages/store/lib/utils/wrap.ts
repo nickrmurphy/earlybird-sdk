52f3b541d006a2066fb490e0d81f006597be5101
nickrmurphy <hi@nickmurphy.io>
2025-06-14 20:36:01 -0500

    Add CRDT field type and doc utilities

--- packages/store/lib/utils/wrap.ts
+++ packages/store/lib/utils/wrap.ts
@@ -1,23 +1,44 @@
 import type { StandardSchemaV1 } from '../../store/schema.types';
-import type { CRDTDocument } from '../store';
-
-import { generateHLC } from './hlc';
+import type { CRDTDoc, CRDTField } from '../store';
+import type { Clock } from './hlc';
 
 export function wrap<T extends StandardSchemaV1>(
-	obj: StandardSchemaV1.InferOutput<T>,
-): CRDTDocument<T> {
-	const crdtDoc: CRDTDocument<T> = {};
+	data: StandardSchemaV1.InferOutput<T>,
+	clock: Clock,
+): CRDTField<T> {
+	return {
+		_value: data,
+		_hlc: clock.tick(),
+	};
+}
 
-	if (typeof obj !== 'object' || Array.isArray(obj)) {
-		throw new Error('Invalid object. Wrappable objects must be plain objects.');
+export function wrapObject<T extends StandardSchemaV1>(
+	data: { [key: string]: StandardSchemaV1.InferOutput<T> },
+	clock: Clock,
+): CRDTDoc<T> {
+	const doc: { [key: string]: CRDTField<T> } = {};
+
+	for (const [key, value] of Object.entries(data)) {
+		doc[key] = wrap(value, clock);
 	}
 
-	for (const [path, value] of Object.entries(obj as Record<string, unknown>)) {
-		crdtDoc[path] = {
-			_value: value,
-			_hlc: generateHLC(),
-		};
+	return doc;
+}
+
+export function unwrap<T extends StandardSchemaV1>(
+	field: CRDTField<T>,
+): StandardSchemaV1.InferOutput<T> {
+	return field._value;
+}
+
+export function unwrapObject<T extends StandardSchemaV1>(
+	doc: CRDTDoc<T>,
+): { [key: string]: StandardSchemaV1.InferOutput<T> } {
+	const data: { [key: string]: StandardSchemaV1.InferOutput<T> } = {};
+
+	for (const [key, field] of Object.entries(doc)) {
+		data[key] = unwrap(field);
 	}
 
-	return crdtDoc;
+	return data;
 }
