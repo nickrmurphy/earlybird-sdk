fd75fa1631b6e19265004287092afd37ee263b5e
nickrmurphy <117491495+nickrmurphy@users.noreply.github.com>
2025-06-20 19:17:29 -0500

    Burn it down

--- packages/store/lib/storage/types.ts
+++
@@ -1,8 +0,0 @@
-export type StorageAdapter = {
-	loadData: () => Promise<string | null>;
-	saveData: (data: string) => Promise<void>;
-	loadHLC: () => Promise<string | null>;
-	saveHLC: (data: string) => Promise<void>;
-	registerListener: (key: string, listener: () => void) => void;
-	unregisterListener: (key: string) => void;
-};
